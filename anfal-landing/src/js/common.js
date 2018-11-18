9c72110fda57d04b25df6a4bcafdac072cb40773
Azat Mannanov <maaz1313@gmail.com>
2018-11-18 14:40:34 +0500

    Second part of the page is done

---
+++ anfal-landing/src/js/common.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+  /* Slider */
+
+  $(".slider").slick({
+    slidesToShow: 5,
+    centerMode: true,
+    centerPadding: '0',
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        initialSlide: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        initialSlide: 2,
+      }
+    },
+  ]
+  });
+
+  new WOW(
+    {
+      mobile:false,
+    }
+  ).init();
+
+  /* popup */
+
+  $(".header-content__btn").on("click", function() {
+    $(".overlay").show();
+    $("#video-popup").show();
+  });
+
+// Закрыть Popup
+
+  $(".popup__close").on("click", function() {
+    $(".overlay, #video-popup").hide();
+  });
+
+});
+
+
